832e6dbb9abbaea67fb397e04bfd1c9049a0a943
Cesc <vallcferre@gmail.com>
2025-12-31 19:29:14 +0100

    Adding structure of project

---
+++ src/main.py
@@ -0,0 +1,24 @@
+import argparse
+import src.modules.ex1_package.load_data as load_data
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="Execució dels exercicis de la PAC"
+    )
+
+    parser.add_argument(
+        "-ex",
+        type=int,
+        help="Executa els exercicis des del 1 fins al número indicat"
+    )
+
+    args = parser.parse_args()
+
+    if args.ex is None:
+        print(load_data.patata())
+    else:
+        limit = args.ex
+        print(limit)
+
+
